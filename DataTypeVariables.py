13f029ccc05c6cdf7f972f339a1c0034d2601d02
Tariq Mehmood <tariqmehmoodsthp23@gmail.com>
2025-02-01 18:04:15 +0500

    DataType_Variables
    This is first day work

---
+++ DataTypeVariables.py
@@ -0,0 +1,31 @@
+
+print("Hello world")
+print("This is my first practise ")
+name ="Tariq"
+name2='Tariq_Gul'
+name3="'Tariq Gul Indhar'"
+age=20
+heighet=22.5
+isitMarried=False
+gender='M'
+a=None
+n1=5
+n2=6
+sum=n1+n2
+minus=n1-n2
+print(type(name))
+print(type(age))
+print(type(heighet))
+print(type(isitMarried))
+print(type(gender))
+print(type(a))
+print("My Name is: ",name)
+print("My Name is:  ",name2)
+print("My Name is : ",name3)
+print("My Age is ",age)
+print("My heighet is ",heighet)
+print("Am I married  ?  ",isitMarried)
+print("My gender is : ",gender)
+print("Have U any Question ? ",a)
+print("The sum Of Two Number is  :  ",sum)
+print("The Differenec Of Two Number is (n1-n2)  :  ",minus)
