c5212d3b144db0296206156f9433c2df52f6a906
Tariq Mehmood <tariqmehmoodsthp23@gmail.com>
2025-02-16 00:14:36 +0500

    Create List.py

---
+++ List.py
@@ -0,0 +1,17 @@
+
+#List
+#A built-in data type that stores set of values
+
+#It can store elements of different types (integer, float, string, etc.)
+
+My_list=[]
+
+roll_No=[11,22,33,44,55,66]
+fruits=["apple","Banana","Cherry","Dates"]
+marks = [87, 64, 33, 95, 76]
+student=["Tariq",3.5,21,True]
+
+
+#Accesing element 
+print(marks)
+print(student[0])
